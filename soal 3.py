793f3347c8c843bf82e7e34caa8a53cdd5f03b32
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 16:48:56 +0330

    Update soal 3.py

--- soal 3.py
+++ soal 3.py
@@ -1,5 +1,9 @@
 n = str(input())
-if '3' and '5' and '7' in n :
-    print('True')
+if '3' and '5' and '7' in n:
+    if not '3' and '5' and '7' in n:
+        print('True')
+    else :
+        print('False')
 else :
     print('False')
+    
