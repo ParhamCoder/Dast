a6da34b0dccad48b73209057ef265a4afa2519e1
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 18:11:42 +0330

    Update soal 3.py

--- soal 3.py
+++ soal 3.py
@@ -1,13 +1,9 @@
 def diff_operation(num):
-    # تبدیل عدد به لیست اعداد
     digits = list(str(num))
-    # مرتب کردن اعداد به صورت صعودی و نزولی
     asc_digits = sorted(digits)
     desc_digits = sorted(digits, reverse=True)
-    # تبدیل اعداد به عدد
     asc_num = int(''.join(asc_digits))
     desc_num = int(''.join(desc_digits))
-    # محاسبه عدد اختلافی
     diff = desc_num - asc_num
     return diff
 
@@ -16,11 +12,7 @@ def get_final_diff(num, k):
     for _ in range(k):
         final_diff = diff_operation(final_diff)
     return final_diff
-
-# دریافت ورودی از کاربر
-num = int(input("عدد اولیه را وارد کنید: "))
-k = int(input("تعداد مراحل مورد نظر را وارد کنید: "))
-
-# محاسبه عدد اختلافی نهایی
+num = int(input("First num: "))
+k = int(input("Count: "))
 result = get_final_diff(num, k)
-print("عدد اختلافی نهایی:", result)
+print("total :", result)
