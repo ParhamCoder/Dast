82a0c55c16874500bcfc5ed316ae8321e71c03de
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 17:23:35 +0330

    Update soal 3.py
    .

--- soal 3.py
+++ soal 3.py
@@ -1,21 +1,16 @@
-n = str(input())
-if '3' and '5' and '7' in n:
-    a = True
-    for i in n:
-        if i not in ['3', '5', '7']:
-            a = False
-else:
-    a = False
-
-x = 0
-n = int(n)
-for j in range(n):
-    j = str(j)
-    if '3' and '5' and '7' in j:
-        x += 1
-        for i in j:
+def number_no (k) :
+    n = str(k)
+    if '3' in n and '5' in n and '7' in n :
+        a = True
+        for i in n:
             if i not in ['3', '5', '7']:
-                x += 0
-    else:
-        x += 0
-print(x)
+                a = False
+    else :
+        a = False
+
+def count_num (a) :
+    count = 0
+    for k in a:
+        if number_no (k) :
+            count += 1
+        return count
