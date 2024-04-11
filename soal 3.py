bc93d0bf4552bd225744166b8aa7deef2235a53b
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 17:02:45 +0330

    Update soal 3.py

--- soal 3.py
+++ soal 3.py
@@ -1,18 +1,11 @@
-def is_sacred(num):
-    num_str = str(num)
-    if '3' in num_str and '5' in num_str and '7' in num_str:
-        for digit in num_str:
-            if digit not in ['3', '5', '7']:
-                return False
-        return True
+n = str(input())
+if '3' and '5' and '7' in n:
+    a = True
+    for i in n:
+        if i not in ['3', '5', '7']:
+            a = False
+else:
+    a = False
+print(a)
 
-def count_sacred_numbers(n):
-    count = 0
-    for i in range(1, n+1):
-        if is_sacred(i):
-            count += 1
-    return count
 
-n = int(input())
-result = count_sacred_numbers(n)
-print(result)
