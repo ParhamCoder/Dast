d328dda60b55b0b15ac8e9c0f628e45ce01de11b
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 18:01:30 +0330

    Update soal 3.py

--- soal 3.py
+++ soal 3.py
@@ -1,33 +1,25 @@
 a = str(input())
 k = int(input())
-b = a
-x = a
-y = 0
-while not y == k:
-    a = str(a)
-    b = str(b)
-    a = x
-    a = sorted(a)
-    b = sorted(a)
-    b.sort(reverse=True)
-    def stringa(a):
-        text = ''
-        for i in a:
-            text = text + str(i)
-        return text
-    def stringb(b):
-        text = ''
-        for i in a:
-            text = text + str(i)
-        return text
+a = sorted(a)
+b = sorted(a)
+b.sort(reverse=True)
+# def stringa(a):
+#     text = ''
+#     for i in a:
+#         text = text + str(i)
+#     return text
+# def stringb(b):
+#     text = ''
+#     for i in b:
+#         text = text + str(i)
+#     return text
 
-    a = stringa(a)
-    b = stringb(b)
-    a = int(a)
-    b = int(b)
-    x = b - a
+
+for j in range(1, (k+1)) :
+    v = a - b
+    
 
 
 
-
+x = b-a
 print(x)
