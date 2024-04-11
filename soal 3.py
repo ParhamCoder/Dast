6b06b5fb2ee14fc6be585af8aaf9fd02f4f90ddf
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 17:12:49 +0330

    Update soal 3.py

--- soal 3.py
+++ soal 3.py
@@ -6,6 +6,16 @@ if '3' and '5' and '7' in n:
             a = False
 else:
     a = False
-print(a)
 
-
+x = 0
+n = int(n)
+for j in range(n):
+    j = str(j)
+    if '3' and '5' and '7' in j:
+        x += 1
+        for i in j:
+            if i not in ['3', '5', '7']:
+                x += 0
+    else:
+        x += 0
+print(x)
