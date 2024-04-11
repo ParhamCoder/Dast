502dffaf1ba7d32b2496a3ef3d98db70cf0f8e3a
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 17:47:09 +0330

    Update soal 3.py

--- soal 3.py
+++ soal 3.py
@@ -1,18 +1,13 @@
-def moghadas(num):
-    num1 = str(num)
-    if '3' in num1 and '5' in num1 and '7' in num1:
-        for i in num1:
-            if i not in ['3', '5', '7']:
-                return False
-        return True
-
-def shomaresh(n):
-    x = 0
-    for i in range(1, n+1):
-        if moghadas(i):
-            x += 1
-    return x
-
-n = int(input())
-result = shomaresh(n)
-print(result)
+b = sorted(a)
+b.sort(reverse=True)
+def stringa(a):
+    text = ''
+    for i in a:
+        text = text + str(i)
+    return text
+def stringb(b):
+    text = ''
+    for i in a:
+        text = text + str(i)
+    return text
+    
