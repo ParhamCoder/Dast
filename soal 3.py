85ca207a8c77bf7d3a14553a26259c4ced568b7b
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 18:07:27 +0330

    Update soal 3.py

--- soal 3.py
+++ soal 3.py
@@ -1,20 +1,26 @@
-a = str(input())
-k = int(input())
-a = sorted(a)
-b = sorted(a)
-b.sort(reverse=True)
-# def stringa(a):
-#     text = ''
-#     for i in a:
-#         text = text + str(i)
-#     return text
-# def stringb(b):
-#     text = ''
-#     for i in b:
-#         text = text + str(i)
-#     return text
+def diff_operation(num):
+    # تبدیل عدد به لیست اعداد
+    digits = list(str(num))
+    # مرتب کردن اعداد به صورت صعودی و نزولی
+    asc_digits = sorted(digits)
+    desc_digits = sorted(digits, reverse=True)
+    # تبدیل اعداد به عدد
+    asc_num = int(''.join(asc_digits))
+    desc_num = int(''.join(desc_digits))
+    # محاسبه عدد اختلافی
+    diff = desc_num - asc_num
+    return diff
 
+def get_final_diff(num, k):
+    final_diff = num
+    for _ in range(k):
+        final_diff = diff_operation(final_diff)
+    return final_diff
 
-for j in range(1, (k+1)) :
-    v = a - b
+# دریافت ورودی از کاربر
+num = int(input("عدد اولیه را وارد کنید: "))
+k = int(input("تعداد مراحل مورد نظر را وارد کنید: "))
 
+# محاسبه عدد اختلافی نهایی
+result = get_final_diff(num, k)
+print("عدد اختلافی نهایی:", result)
