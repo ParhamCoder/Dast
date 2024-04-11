75cfdaad70294ce953722f30814c2028cb42e117
Portal <166227618+ParhamCoder@users.noreply.github.com>
2024-04-11 17:58:12 +0330

    Update soal 3.py

--- soal 3.py
+++ soal 3.py
@@ -1,20 +1,24 @@
 a = str(input())
+k = int(input())
 a = sorted(a)
 b = sorted(a)
 b.sort(reverse=True)
-def stringa(a):
-    text = ''
-    for i in a:
-        text = text + str(i)
-    return text
-def stringb(b):
-    text = ''
-    for i in a:
-        text = text + str(i)
-    return text
+# def stringa(a):
+#     text = ''
+#     for i in a:
+#         text = text + str(i)
+#     return text
+# def stringb(b):
+#     text = ''
+#     for i in b:
+#         text = text + str(i)
+#     return text
 
-a = stringa(a)
-b = stringb(b)
+
+for j in range(1, (k+1)) :
+    v = a - b
+    
+    
 
 x = b-a
 print(x)
